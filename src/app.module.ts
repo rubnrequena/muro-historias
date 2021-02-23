7f17ba11d06da52a4680d37abff2a7ea079956ba
Ruben Requena <rubnrequena@gmail.com>
2021-02-23 12:11:13 +0100

    conectar a mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { NotasModule } from './notas/notas.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [UsuariosModule, NotasModule],
+  imports: [UsuariosModule, NotasModule, MongooseModule.forRoot('mongodb://localhost:27017/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
