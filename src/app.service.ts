c70d73c7f2611dac3ea78c78bb20cbfc47adf216
Ruben Requena <rubnrequena@gmail.com>
2021-02-24 21:35:22 +0100

    lint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hola, bienvenido al Muro de la Fama, dejanos aqui tus mejores ideas, nosotros las cuidaremos por ti. También podras compartir las ideas con el resto del mundo!';
   }
 }
